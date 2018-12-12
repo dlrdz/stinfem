1d000d9f71b930511e70135ffcff7a2c7d329d0c
keremsan <kerem@voltlines.com>
2018-12-12 15:47:34 +0300

    added teacher to infront of urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from profiles.views import teacher
 
 urlpatterns = [
-    url(r'^teacher/$', teacher.student_list, name='student_list'),
-    url(r'^(?P<pk>[0-9]+)/$', teacher.student_detail, name='detail'),
+    url(r'^teacher/students/$', teacher.student_list, name='student_list'),
+    url(r'^teacher/students/(?P<pk>[0-9]+)/$',
+        teacher.student_detail, name='detail'),
 
 
 ]
